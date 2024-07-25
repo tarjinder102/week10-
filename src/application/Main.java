ae67b88f3e4f156871eed2ea9afadbde1454d626
lalta <lalta@LAPTOP-7Q5O4E69.localdomain>
2024-07-25 19:33:15 -0400

    this is week 10 lab

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,8 +13,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -22,7 +23,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 
-public class Main extends Application {
+public class main extends Application {
 	
 	
 	private static final Logger logger = Logger.getLogger(Main.class.getName());
@@ -39,43 +40,79 @@ public class Main extends Application {
 
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
+		grid.setHgap(15);
+		grid.setVgap(15);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 
-		Text scenetitle = new Text("Add User");
+		Text scenetitle = new Text("Student registration form");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(scenetitle, 0, 0, 2, 1);
 
-		Label userNameLabel = new Label("Username:");
-		grid.add(userNameLabel, 0, 1);
+		 Label fullNameLabel = new Label("Full Name:");
+	        grid.add(fullNameLabel, 0, 1);
 
-		TextField usernameTextField = new TextField();
-		grid.add(usernameTextField, 1, 1);
+	        TextField fullNameTextField = new TextField();
+	        grid.add(fullNameTextField, 1, 1);
 
-		Label lastNameLabel = new Label("Last Name:");
-		grid.add(lastNameLabel, 0, 2);
+	        Label lastNameLabel = new Label("Last Name:");
+	        grid.add(lastNameLabel, 0, 2);
 
-		TextField lastNameTextField = new TextField();
-		grid.add(lastNameTextField, 1, 2);
+	        TextField lastNameTextField = new TextField();
+	        grid.add(lastNameTextField, 1, 2);
 
-		Label firstNameLabel = new Label("First Name:");
-		grid.add(firstNameLabel, 0, 3);
+	        Label emailLabel = new Label("Email:");
+	        grid.add(emailLabel, 0, 3);
 
-		TextField firstNameTextField = new TextField();
-		grid.add(firstNameTextField, 1, 3);
+	        TextField emailTextField = new TextField();
+	        grid.add(emailTextField, 1, 3);
 
-		Label passwordLabel = new Label("Password:");
-		grid.add(passwordLabel, 0, 4);
+	        Label dobLabel = new Label("Date of Birth:");
+	        grid.add(dobLabel, 0, 4);
 
-		PasswordField passwordField = new PasswordField();
-		grid.add(passwordField, 1, 4);
+	        TextField dobTextField = new TextField();
+	        grid.add(dobTextField, 1, 4);
 
-		Button saveButton = new Button("Save");
+	        Label userNameLabel = new Label("Username:");
+	        grid.add(userNameLabel, 0, 5);
+
+	        TextField usernameTextField = new TextField();
+	        grid.add(usernameTextField, 1, 5);
+
+	        Label passwordLabel = new Label("Password:");
+	        grid.add(passwordLabel, 0, 6);
+
+	        PasswordField passwordField = new PasswordField();
+	        grid.add(passwordField, 1, 6);
+
+	        Label genderLabel = new Label("Gender:");
+	        grid.add(genderLabel, 0, 7);
+
+	        ToggleGroup genderGroup = new ToggleGroup();
+
+	        RadioButton maleRadio = new RadioButton("Male");
+	        maleRadio.setToggleGroup(genderGroup);
+
+	        RadioButton femaleRadio = new RadioButton("Female");
+	        femaleRadio.setToggleGroup(genderGroup);
+
+	        RadioButton otherRadio = new RadioButton("Other");
+	        otherRadio.setToggleGroup(genderGroup);
+
+	        HBox genderBox = new HBox(10);
+	        genderBox.getChildren().addAll(maleRadio, femaleRadio, otherRadio);
+	        grid.add(genderBox, 1, 7);
+
+	        Label courseLabel = new Label("Course:");
+	        grid.add(courseLabel, 0, 8);
+
+	        TextField courseTextField = new TextField();
+	        grid.add(courseTextField, 1, 8);
+
+		Button saveButton = new Button("Register");
 		HBox hBox = new HBox(10);
-		hBox.setAlignment(Pos.BOTTOM_RIGHT);
+		hBox.setAlignment(Pos.BOTTOM_LEFT);
 		hBox.getChildren().add(saveButton);
-		grid.add(hBox, 1, 5);
+		grid.add(hBox, 1, 9);
 		
 		
 		/*
@@ -86,7 +123,7 @@ public class Main extends Application {
 
 			String username = usernameTextField.getText().trim();
 			String lastName = lastNameTextField.getText().trim();
-			String firstName = firstNameTextField.getText().trim();
+			String firstName = fullNameTextField.getText().trim();
 			String password = passwordField.getText();
 			
 			if (!StringPool.BLANK.equals(username) && !StringPool.BLANK.equals(lastName)
